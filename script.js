fd181ff1db5c6b8ed21dba6969eb4f5bdfc9a208
studern-bzz <studern@bzz.ch>
2023-10-02 14:07:48 +0200

    stand nachmittagspause MO

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {document.getElementById('1');
+
+    document.getElementById("1").addEventListener('click', function () {
+        alert('Hallo Welt');
+    });
+})
